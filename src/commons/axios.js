18acce16c1fe1168bec48901cc9dc09ba21370b6
59417 <sun313131@gmail.com>
2022-03-02 05:46:00 +0800

    first commit

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -3,12 +3,11 @@ import _axios from 'axios';
 const axios = (baseURL) => {  // 可以給定 baseURL
     
     const instance = _axios.create({
-        baseURL: 
-            baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3003',  
+        baseURL: 'https://react-store-api-m3smd3v7v-59417.vercel.app',
+            //baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3003',  
             // 給定baseURL 或 開發環境url 或 生產環境本地3003
         timeout: 1000
     });
-    console.log()
 
     instance.interceptors.request.use(  // 攔截 request
         (config) => {  // req 送出前執行下方命令
